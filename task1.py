b5d6825013bc25f4048c99e32ca5064009c5a4ad
Neila16 <240625@astanait.edu.kz>
2025-12-16 17:36:15 +0500

    First task

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
-firstname, lastname = input("Your last name, first name?").split()
+lastname, firstname = input("Your last name, first name?").split()
 age = input("How old are you?")
 phonenumber = input("Your phone number?") 
-print("Your first name, last name:", firstname, lastname) 
+print("Your first name, last name:", lastname, firstname) 
 print("Your age:", age)
 print("Your phone number:", phonenumber)
