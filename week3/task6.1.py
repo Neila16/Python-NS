de56f83928196102d130eea74299e621ce4e8352
Neila16 <240625@astanait.edu.kz>
2025-12-30 17:00:58 +0500

    6.1

---
+++ week3/task6.1.py
@@ -0,0 +1,14 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+a, b = map(int, input().split())
+g = gcd(a, b)
+l = a * b // g
+
+print("GCD:", g)
+print("LCM:", l)
+
+
+
